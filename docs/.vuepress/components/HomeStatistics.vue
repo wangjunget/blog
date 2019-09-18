1eeb709a4d940ac110a4e14aff3b26ade5ee7c72
wangjunget <wangjunget@163.com>
2019-09-18 08:00:56 +0800

    饼图添加标签

--- docs/.vuepress/components/HomeStatistics.vue
+++ docs/.vuepress/components/HomeStatistics.vue
@@ -9,6 +9,7 @@ const echarts = require('echarts/lib/echarts')
 require('echarts/lib/chart/pie')
 require('echarts/lib/component/title')
 require('echarts/lib/component/tooltip')
+require('echarts/lib/component/legend')
 
 
 export default {
